800ee316151b5c2d8db2bec27d4c6b2f0e20755e
carllin <carl@anza.xyz>
2025-04-03 22:01:57 -0500

    Make supermajority root the root (#136)

--- core/src/alpenglow_consensus/voting_loop.rs
+++ core/src/alpenglow_consensus/voting_loop.rs
@@ -782,8 +782,7 @@ impl VotingLoop {
             &ctx.leader_schedule_cache,
             accounts_background_request_sender,
             &ctx.rpc_subscriptions,
-            // TODO: figure out a sufficient range for rpcs
-            Some(old_root),
+            Some(new_root),
             // TODO(ashwin): AG doesn't need most of these
             // we should stop populating them after the migration
             // and remove them here
