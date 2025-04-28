e2ac1dd45d37ff75599b2eb55eca81a3fb5839a8
Wen <113942165+wen-coding@users.noreply.github.com>
2025-04-28 08:31:05 -0700

    Remove unnecessary has_same_prev_vote. (#170)

--- core/src/alpenglow_consensus/certificate_pool.rs
+++ core/src/alpenglow_consensus/certificate_pool.rs
@@ -393,7 +393,7 @@ impl<VC: VoteCertificate> CertificatePool<VC> {
         if self
             .vote_pools
             .get(&(slot, VoteType::Skip))
-            .is_some_and(|pool| pool.has_same_prev_vote(my_pubkey, None, None))
+            .is_some_and(|pool| pool.has_prev_vote(my_pubkey))
         {
             return true;
         }
