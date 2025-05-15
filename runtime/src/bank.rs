41eb77422c0169d2cee0a8d74e7250d2fbf75004
Ashwin Sekar <ashwin@anza.xyz>
2025-05-15 08:01:37 -0400

    voting loop: add fallback vote variants (#189)

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -6810,6 +6810,10 @@ impl Bank {
         &self.fee_structure
     }
 
+    pub fn parent_block_id(&self) -> Option<Hash> {
+        self.parent().and_then(|p| p.block_id())
+    }
+
     pub fn block_id(&self) -> Option<Hash> {
         *self.block_id.read().unwrap()
     }
