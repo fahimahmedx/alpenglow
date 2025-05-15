1334a8bc1ba2e5592a465fc6d72e45cc776970c0
Wen <113942165+wen-coding@users.noreply.github.com>
2025-05-15 09:47:39 -0400

    Use the bls_pubkey_to_rank_map in EpochStakes. (#191)

--- core/src/alpenglow_consensus/transaction.rs
+++ core/src/alpenglow_consensus/transaction.rs
@@ -1,11 +1,11 @@
-use solana_sdk::transaction::VersionedTransaction;
+use {solana_bls::keypair::Keypair as BLSKeypair, solana_sdk::transaction::VersionedTransaction};
 
 pub trait AlpenglowVoteTransaction: Clone + std::fmt::Debug {
-    fn new_for_test() -> Self;
+    fn new_for_test(bls_keypair: BLSKeypair) -> Self;
 }
 
 impl AlpenglowVoteTransaction for VersionedTransaction {
-    fn new_for_test() -> Self {
+    fn new_for_test(_bls_keypair: BLSKeypair) -> Self {
         Self::default()
     }
 }
