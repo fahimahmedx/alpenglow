e2a4bedcf7ce209b42ee07a1ba463e972f81e32e
Ashwin Sekar <ashwin@anza.xyz>
2025-06-25 18:37:27 -0400

    blockstore: purge slot_certificates column (#229)

--- ledger/src/blockstore/blockstore_purge.rs
+++ ledger/src/blockstore/blockstore_purge.rs
@@ -315,6 +315,10 @@ impl Blockstore {
             & self
                 .merkle_root_meta_cf
                 .delete_range_in_batch(write_batch, from_slot, to_slot)
+                .is_ok()
+            & self
+                .slot_certificates_cf
+                .delete_range_in_batch(write_batch, from_slot, to_slot)
                 .is_ok();
 
         match purge_type {
@@ -394,6 +398,10 @@ impl Blockstore {
                 .is_ok()
             & self
                 .merkle_root_meta_cf
+                .delete_file_in_range(from_slot, to_slot)
+                .is_ok()
+            & self
+                .slot_certificates_cf
                 .delete_file_in_range(from_slot, to_slot)
                 .is_ok()
     }
