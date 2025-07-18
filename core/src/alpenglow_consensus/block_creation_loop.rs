ce74d80c92455d105a3d8f9e2643ec7dd24c3b8f
fahimahmedx <f92ahmed@uwaterloo.ca>
2025-07-17 22:30:12 -0700

    rename to BlockCreationLoopMetrics

--- core/src/alpenglow_consensus/block_creation_loop.rs
+++ core/src/alpenglow_consensus/block_creation_loop.rs
@@ -91,13 +91,13 @@ pub struct LeaderWindowNotifier {
 }
 
 #[derive(Default)]
-struct BlockCreationLoopTiming {
+struct BlockCreationLoopMetrics {
     last_submit: u64,
     loop_count: u64,
     replay_is_behind_count: u64,
 }
 
-impl BlockCreationLoopTiming {
+impl BlockCreationLoopMetrics {
     fn update(&mut self, replay_is_behind_count: u64) {
         self.loop_count += 1;
         self.replay_is_behind_count = replay_is_behind_count;
@@ -110,12 +110,12 @@ impl BlockCreationLoopTiming {
 
         if elapsed_ms > 1000 {
             datapoint_info!(
-                "block-creation-loop-timing-stats",
+                "block-creation-loop-metrics",
                 ("loop_count", self.loop_count, i64),
                 ("replay_is_behind_count", self.replay_is_behind_count, i64),
             );
             
-            *self = BlockCreationLoopTiming::default();
+            *self = BlockCreationLoopMetrics::default();
             self.last_submit = now;
         }
     }
@@ -215,7 +215,7 @@ pub fn start_loop(config: BlockCreationLoopConfig) {
         replay_highest_frozen,
     };
 
-    let mut timing = BlockCreationLoopTiming::default();
+    let mut metrics = BlockCreationLoopMetrics::default();
 
     // Setup poh
     reset_poh_recorder(&ctx.bank_forks.read().unwrap().working_bank(), &ctx);
@@ -269,7 +269,7 @@ pub fn start_loop(config: BlockCreationLoopConfig) {
             first_in_leader_window(start_slot),
             "{start_slot} was not first in leader window but voting loop notified us"
         );
-        if let Err(e) = start_leader_retry_replay(start_slot, parent_slot, skip_timer, &ctx, &mut timing) {
+        if let Err(e) = start_leader_retry_replay(start_slot, parent_slot, skip_timer, &ctx, &mut metrics) {
             // Give up on this leader window
             error!(
                 "{my_pubkey}: Unable to produce first slot {start_slot}, skipping production of our entire leader window \
@@ -332,13 +332,13 @@ pub fn start_loop(config: BlockCreationLoopConfig) {
 
             // Although `slot - 1`has been cleared from `poh_recorder`, it might not have finished processing in
             // `replay_stage`, which is why we use `start_leader_retry_replay`
-            if let Err(e) = start_leader_retry_replay(slot, slot - 1, skip_timer, &ctx, &mut timing) {
+            if let Err(e) = start_leader_retry_replay(slot, slot - 1, skip_timer, &ctx, &mut metrics) {
                 error!("{my_pubkey}: Unable to produce {slot}, skipping rest of leader window {slot} - {end_slot}: {e:?}");
                 break;
             }
         }
-        timing.loop_count += 1;
-        timing.maybe_submit();
+        metrics.loop_count += 1;
+        metrics.maybe_submit();
     }
 
     receive_record_loop.join().unwrap();
@@ -377,7 +377,7 @@ fn start_leader_retry_replay(
     parent_slot: Slot,
     skip_timer: Instant,
     ctx: &LeaderContext,
-    timing: &mut BlockCreationLoopTiming,
+    metrics: &mut BlockCreationLoopMetrics,
 ) -> Result<(), StartLeaderError> {
     let my_pubkey = ctx.my_pubkey;
     let timeout = block_timeout(leader_slot_index(slot));
@@ -387,7 +387,7 @@ fn start_leader_retry_replay(
                 return Ok(());
             }
             Err(StartLeaderError::ReplayIsBehind(_)) => {
-                timing.replay_is_behind_count += 1;
+                metrics.replay_is_behind_count += 1;
                 
                 trace!(
                     "{my_pubkey}: Attempting to produce slot {slot}, however replay of the \
